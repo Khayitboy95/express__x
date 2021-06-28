2b23d93201b2b4cddac8b60cb7926ff5046719e3
unknown <ergashevx33@gmail.com>
2021-06-28 22:58:49 +0500

    middleware

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const express = require('express');
-
+const Joi = require('joi');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const helmet = require('helmet');
+const morgan = require('morgan');
+
 app.use(express.json());
+app.use(helmet());
+app.use(morgan('tiny'));
 
 const books = [
     {id: 1, name: "rich dad poor dad"},
     {id: 2, name: "good to great"},
     {id: 3, name: "rework"}
-]
+];
 
 app.get('/', (req, res) => {
     res.send('expres');
 });
 
 app.get('/api/books', (req, res) => {
-    res.send(['rich dad poor dad', 'the war of art', 'good to great']);
+    res.send(books);
 });
 
 app.post('/api/books', (req, res) => {
+
+    const {error} = validateBook(req.body);
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
     const book = {
         id: books.length + 1,
         name: req.body.name
@@ -30,14 +41,43 @@ app.post('/api/books', (req, res) => {
 app.get('/api/books/:id', (req, res) => {
     const book = books.find( b => b.id === parseInt(req.params.id));
     if(!book){
-        res.status(404).send('Berilgan IDga teng bo\'lgan kitob topilmadi');
+        return res.status(404).send('Berilgan IDga teng bo\'lgan kitob topilmadi');
     }
     res.send(book);
 });
  
-app.get('/api/articles/:year/:month', (req, res) => {
-    res.send(req.params);
+app.put('/api/books/:id', (req, res) => {
+    const book = books.find( b => b.id === parseInt(req.params.id));
+    if(!book){
+        return res.status(404).send('Berilgan IDga teng bo\'lgan kitob topilmadi');
+    }
+
+    const {error} = validateBook(req.body);
+
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    
+    book.name = req.body.name;
+    res.send(book);
 });
+
+app.delete('/api/books/:id', (req, res) => {
+    const book = books.find( b => b.id === parseInt(req.params.id));
+    if(!book){
+        return res.status(404).send('Berilgan IDga teng bo\'lgan kitob topilmadi');
+    }
+    const bookIndex = books.indexOf(book);
+    books.splice(bookIndex, 1);
+    res.send(book);
+});
+
+function validateBook(book){
+    const bookSchema = Joi.object({
+        name: Joi.string().required().min(3)
+    });
+    return bookSchema.validate(book);
+}
 
 app.listen(5000, () => {
     console.log(`${PORT}chi portni eshitishni boshladim`);
