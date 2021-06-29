2771d86ef20fde87346208d2f67a648b84f93201
unknown <ergashevx33@gmail.com>
2021-06-29 22:28:55 +0500

    add config files

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const helmet = require('helmet');
 const morgan = require('morgan');
+const config = require('config');
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(helmet());
+app.use(express.static('public'));
 app.use(morgan('tiny'));
+
+console.log(config.get('mailserver'))
 
 const books = [
     {id: 1, name: "rich dad poor dad"},
